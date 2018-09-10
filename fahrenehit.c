088febb39992baa9bad28a960cee8f016ff40d7a
Emma Sanders <19sanders@stfrancishs.org>
2018-09-10 19:16:04 +0000

    init

--- fahrenehit.c
+++ fahrenehit.c
@@ -4,7 +4,7 @@ int main (void)
 {
     int fahr;
 
-    for (fahr = -100; fahr <= 100; fahr = fahr +10)
+    for (fahr = -50; fahr <= 120; fahr = fahr +5)
     {
             printf ("f - %3d c - %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
     }
